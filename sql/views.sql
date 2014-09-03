1e6a276594517de0aa693e507c4d7eb0dbfa3c3d
Vivek Singh <petmongrels@gmail.com>
2014-09-03 18:20:40 +0530

    new view for concept answers queries for chief complaints

--- sql/views.sql
+++ sql/views.sql
@@ -12,6 +12,18 @@ and concept.date_retired is null
 and concept_name.voided = false
 and concept_name.concept_name_type = 'FULLY_SPECIFIED';
 
+create or replace view concept_children_view as
+select
+parent.concept_id as parent_concept_id,
+parent.concept_class_name as parent_concept_class_name,
+parent.concept_name as parent_concept_name,
+child.concept_id as child_concept_id,
+child.concept_class_name as child_concept_class_name,
+child.concept_name as child_concept_name
+from concept_view parent, concept_view child, concept_set
+where parent.concept_id = concept_set.concept_set and
+child.concept_id = concept_set.concept_id;
+
 create or replace view concept_answer_view as
 select
 question_concept_view.concept_id as question_concept_id,
@@ -25,10 +37,14 @@ where
 question_concept_view.concept_id = concept_answer.concept_id
 and answer_concept_view.concept_id = concept_answer.answer_concept;
 
-create or replace view text_obs_view as
+create or replace view obs_view as
 select
 concept_view.*,
-obs.value_text as value
+obs.obs_id as obs_id,
+obs.value_text as value_text,
+obs.value_datetime as value_datetime,
+obs.value_numeric as value_numeric,
+obs.obs_group_id as obs_group_id
 from obs, concept_view
 where
 obs.concept_id = concept_view.concept_id;
@@ -43,3 +59,21 @@ from obs, concept_view question, concept_view answer
 where
 obs.concept_id = question.concept_id and
 obs.value_coded = answer.concept_id;
+
+create or replace view radiology_view as
+select
+concept_children_view.child_concept_id as concept_id,
+concept_children_view.child_concept_name as name
+from concept_children_view
+where parent_concept_name = 'Radiology';
+
+-- This would need refactoring when we change the model
+create or replace view radiology_result_view as
+select
+radiology_view.name as name,
+file_obs.value_text as file_name
+from obs_view file_obs, obs_view radiology_obs, radiology_view
+where
+file_obs.concept_name = 'Document' and
+file_obs.obs_group_id = radiology_obs.obs_id and
+radiology_obs.concept_id = radiology_view.concept_id;
