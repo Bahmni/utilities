498bb1b7ff8086f2abc537e2dd82243956f91c84
Deepak N <endeep123@gmail.com>
2014-09-25 10:19:34 +0530

    Do not depend on visit type, use discharge time instead of obs time

--- possible-reports/inpatient_morbidity.sql
+++ possible-reports/inpatient_morbidity.sql
@@ -3,7 +3,6 @@ SET @start_date = '2014-09-01';
 SET @end_date = '2014-10-01';
 
 -- Constants
-SET @ipd_visit_type =  'IPD';
 SET @discharge_encounter_type = 'DISCHARGE';
 SET @report_group_name = 'Inpatient Discharge Reports';
 
@@ -14,13 +13,13 @@ SUM(CASE WHEN person.gender = 'F' THEN 1 ELSE 0 END) AS female,
 SUM(CASE WHEN person.gender = 'M' THEN 1 ELSE 0 END) AS male
 FROM diagnosis_icd10_mapping AS diagnosis
 JOIN valid_confirmed_diagnosis ON valid_confirmed_diagnosis.diagnois_concept_id = diagnosis.concept_id 
-							   AND valid_confirmed_diagnosis.visit_type = @ipd_visit_type
 JOIN person ON valid_confirmed_diagnosis.person_id = person.person_id
-JOIN encounter_data ON encounter_data.patient_id = person.person_id AND encounter_data.encounter_type = @discharge_encounter_type
+JOIN encounter_data as discharge_encounter ON discharge_encounter.visit_id = valid_confirmed_diagnosis.visit_id
+											AND discharge_encounter.encounter_type = @discharge_encounter_type
 JOIN possible_age_group as observed_age_group ON observed_age_group.report_group_name = @report_group_name AND
 					valid_confirmed_diagnosis.obs_datetime BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY)) 
 					AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))  
-WHERE valid_confirmed_diagnosis.obs_datetime BETWEEN @start_date AND @end_date
+WHERE discharge_encounter.encounter_datetime BETWEEN @start_date AND @end_date
 GROUP BY diagnosis.concept_id, diagnosis.full_name, diagnosis.code, observed_age_group.id
 ORDER BY disease;
 
@@ -30,9 +29,10 @@ SUM(CASE WHEN person.gender = 'F' THEN 1 ELSE 0 END) AS female_death,
 SUM(CASE WHEN person.gender = 'M' THEN 1 ELSE 0 END) AS male_death
 FROM diagnosis_icd10_mapping AS diagnosis
 JOIN valid_confirmed_diagnosis ON valid_confirmed_diagnosis.diagnois_concept_id = diagnosis.concept_id 
-							   AND valid_confirmed_diagnosis.visit_type = @ipd_visit_type
-LEFT OUTER JOIN person ON valid_confirmed_diagnosis.person_id = person.person_id AND person.dead = 1
+JOIN person ON valid_confirmed_diagnosis.person_id = person.person_id AND person.dead = 1
 					   AND person.death_date BETWEEN @start_date AND @end_date
-WHERE valid_confirmed_diagnosis.obs_datetime BETWEEN @start_date AND @end_date
+JOIN encounter_data as discharge_encounter ON discharge_encounter.visit_id = valid_confirmed_diagnosis.visit_id
+											AND discharge_encounter.encounter_type = @discharge_encounter_type
+WHERE discharge_encounter.encounter_datetime BETWEEN @start_date AND @end_date
 GROUP BY diagnosis.concept_id, diagnosis.full_name, diagnosis.code
 ORDER BY disease;
--- possible-reports/outpatient_morbidity.sql
+++ possible-reports/outpatient_morbidity.sql
@@ -3,7 +3,7 @@ SET @start_date = '2014-09-01';
 SET @end_date = '2014-10-01';
 
 -- Constants
-SET @opd_visit_type = 'Outpatient';
+SET @admission_encounter_type = 'ADMISSION';
 
 -- Query
 SELECT diagnosis.full_name, diagnosis.code, concept_mapping.term_name AS group_name,
@@ -13,7 +13,9 @@ FROM diagnosis_icd10_mapping AS diagnosis
 LEFT OUTER JOIN concept_mapping ON concept_mapping.concept_id = diagnosis.concept_id
 			AND concept_mapping.source_name = 'ICD 10 - WHO' AND concept_mapping.map_type = 'NARROWER-THAN'
 LEFT OUTER JOIN	valid_confirmed_diagnosis AS patient_diagnosis ON patient_diagnosis.diagnois_concept_id = diagnosis.concept_id 
-	AND patient_diagnosis.visit_type = @opd_visit_type
+	AND NOT EXISTS (SELECT * 
+					FROM encounter_data 
+					WHERE encounter_data.visit_id = patient_diagnosis.visit_id AND encounter_data.encounter_type = @admission_encounter_type)
 	AND (patient_diagnosis.obs_datetime BETWEEN @start_date AND @end_date)
 LEFT OUTER JOIN person ON patient_diagnosis.person_id = person.person_id
 GROUP BY diagnosis.concept_id, diagnosis.full_name, diagnosis.code, concept_mapping.term_name
