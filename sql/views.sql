08ad577e562b74a1a8133b192b65cc4c4e2ac925
Vivek Singh <petmongrels@gmail.com>
2014-09-26 16:17:47 +0530

    modified some views

--- sql/views.sql
+++ sql/views.sql
@@ -1,16 +1,33 @@
+create or replace view concept_reference_view as
+select
+concept_reference_term.concept_reference_term_id as concept_reference_term_id,
+concept_reference_term.name as concept_reference_term_name,
+concept_reference_term.code as concept_reference_term_code,
+concept_reference_source.name as concept_reference_source_name
+from concept_reference_term, concept_reference_source
+where
+concept_reference_term.concept_source_id = concept_reference_source.concept_source_id;
+
 create or replace view concept_view as
 select
 concept.concept_id as concept_id,
 concept_class.name as concept_class_name,
-concept_name.name as concept_name
-from concept, concept_class, concept_name
+concept_name.name as concept_name,
+concept_reference_view.concept_reference_term_code as concept_reference_term_code,
+concept_reference_view.concept_reference_term_name as concept_reference_term_name,
+concept_reference_view.concept_reference_source_name as concept_reference_source_name
+from concept, concept_class, concept_name, concept_reference_map, concept_map_type, concept_reference_view
 where
 concept.class_id = concept_class.concept_class_id
 and concept_name.concept_id = concept.concept_id
 and concept_name.locale = 'en'
 and concept.date_retired is null
 and concept_name.voided = false
-and concept_name.concept_name_type = 'FULLY_SPECIFIED';
+and concept_name.concept_name_type = 'FULLY_SPECIFIED'
+and concept.concept_id = concept_reference_map.concept_id
+and concept_reference_map.concept_map_type_id = concept_map_type.concept_map_type_id
+and concept_map_type.name = 'SAME-AS'
+and concept_reference_map.concept_reference_term_id = concept_reference_view.concept_reference_term_id;
 
 create or replace view concept_children_view as
 select
@@ -44,7 +61,8 @@ obs.obs_id as obs_id,
 obs.value_text as value_text,
 obs.value_datetime as value_datetime,
 obs.value_numeric as value_numeric,
-obs.obs_group_id as obs_group_id
+obs.obs_group_id as obs_group_id,
+obs.person_id as person_id
 from obs, concept_view
 where
 obs.concept_id = concept_view.concept_id;
@@ -53,8 +71,9 @@ create or replace view coded_obs_view as
 select
 question.concept_id as concept_id,
 question.concept_name as concept_name,
+answer.concept_name as concept_value,
 answer.concept_id as answer_concept_id,
-answer.concept_name as value
+obs.obs_group_id as obs_group_id
 from obs, concept_view question, concept_view answer
 where
 obs.concept_id = question.concept_id and
@@ -70,6 +89,7 @@ where parent_concept_name = 'Radiology';
 -- This would need refactoring when we change the model
 create or replace view radiology_result_view as
 select
+file_obs.person_id as person_id,
 radiology_view.name as name,
 file_obs.value_text as file_name
 from obs_view file_obs, obs_view radiology_obs, radiology_view
@@ -77,3 +97,32 @@ where
 file_obs.concept_name = 'Document' and
 file_obs.obs_group_id = radiology_obs.obs_id and
 radiology_obs.concept_id = radiology_view.concept_id;
+
+create or replace view patient_diagnosis_view as
+select
+obs.obs_group_id,
+(case when question_concept.concept_name = 'Coded Diagnosis' then answer_concept.concept_name end) as diagnosis,
+(case when question_concept.concept_name = 'Diagnosis order' then answer_concept.concept_name end) as diagnosis_order,
+(case when question_concept.concept_name = 'Diagnosis Certainty' then answer_concept.concept_name end) as diagnosis_certainty
+from
+obs, concept_view question_concept, concept_view answer_concept
+where
+question_concept.concept_name in ('Coded Diagnosis', 'Diagnosis order', 'Diagnosis Certainty') and
+obs.concept_id = question_concept.concept_id and
+obs.value_coded = answer_concept.concept_id
+group by obs.obs_group_id
+order by obs.obs_group_id;
+
+select distinct(file_name) from radiology_result_view, obs_view
+where radiology_result_view.person_id = obs_view.person_id and
+obs_view.concept_name = 'BMI Status' and
+obs_view.value_text in ('Very Severely Underweight', 'Underweight', 'Severely Underweight') and
+radiology_result_view.name like '%Chest PA%' limit 50
+INTO OUTFILE '/tmp/xray_file_for_low_bmi.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+
+explain  select count(*) from coded_obs_view as diagnosis_obs
+join coded_obs_view as diagnosis_attributes_obs on diagnosis_obs.concept_name = 'Coded Diagnosis' and diagnosis_obs.obs_group_id = diagnosis_attributes_obs.obs_group_id;
