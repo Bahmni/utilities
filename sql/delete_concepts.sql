f352cca2dd7917742c7b859d3b1b6e58c250655c
Vivek Singh <petmongrels@gmail.com>
2014-10-30 20:02:44 +0530

    fixed issues with delete of concepts based on date

--- sql/delete_concepts.sql
+++ sql/delete_concepts.sql
@@ -1,7 +1,18 @@
 SET @cutoff_date = '2014-10-30 00:00:00';
+select count(*) from concept_description where date_created >= @cutoff_date;
+select count(*) from concept_word where concept_id in (select concept_id from concept where date_created >= @cutoff_date);
+select count(*) from concept_word where concept_id in (select concept_id from concept_name where date_created >= @cutoff_date);
+select count(*) from concept_numeric where concept_id in (select concept_id from concept where date_created >= @cutoff_date);
+select count(*) from concept_answer where date_created >= @cutoff_date;
+select count(*) from concept_name where date_created >= @cutoff_date;
+select count(*) from concept where date_created >= @cutoff_date;
+select count(*) from concept_set where date_created >= @cutoff_date;
+select count(*) from concept_reference_map where date_created >= @cutoff_date;
+
+SET @cutoff_date = '2014-10-30 00:00:00';
 delete from concept_description where date_created >= @cutoff_date;
 delete from concept_word where concept_id in (select concept_id from concept where date_created >= @cutoff_date);
-delete from concept_stop_word where concept_id in (select concept_id from concept where date_created >= @cutoff_date);
+delete from concept_word where concept_id in (select concept_id from concept_name where date_created >= @cutoff_date);
 delete from concept_numeric where concept_id in (select concept_id from concept where date_created >= @cutoff_date);
 delete from concept_answer where date_created >= @cutoff_date;
 delete from concept_name where date_created >= @cutoff_date;
